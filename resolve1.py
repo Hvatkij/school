0bb4af05b1b6737e035c2fa5cbee24c5002e9152
Хваткий <ivan-zapara@ya.ru>
2024-05-25 15:00:11 +0300

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,24 @@
+a = 'Практическое задание по уроку "Базовые структуры данных"'
+print(len(a))
+first = 12
+second = 8
+summa = (first + second)
+diff = (first - second)
+summa = str(summa)
+diff = str(first)
+print('Summa: ' + summa)
+print('Diff: ' + diff)
+first = 3
+second = 5
+third = 8
+mean = ((first + second + third)/3)
+mean = str(mean)
+print('Mean: ' + (mean))
+first_string = 'Понедельник'
+second_string = 'Вторник'
+print((first_string) + ', ' + (second_string))
+a = 2
+b = 3
+c = 5
+f = (((a * b) + (a * c)**3)//2)
+print(f)
