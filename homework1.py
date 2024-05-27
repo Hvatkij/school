83387af230ee27162a51dd1634a64cc6afefcfef
Хваткий <ivan-zapara@ya.ru>
2024-05-28 01:33:40 +0300

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,8 +1,8 @@
 example = 'Тапинамбур'
 print(example[0])
 print(example[-1])
-print(example[5:])
+a = len(example)
+b = a//2
+print(example[b:])
 print(example[::-1])
-print(example[-1])
-a = (len(example))
 print(example[1:a:2])
