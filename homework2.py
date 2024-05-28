9f70a5e6bb6002f42c05d13ed2b640f5203518fa
Хваткий <ivan-zapara@ya.ru>
2024-05-28 23:34:41 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -2,5 +2,5 @@ Num_of_compl_DZ = 12
 Num_of_hours = 1.5
 Course_name = 'Python'
 Time_for_one_task = Num_of_hours / Num_of_compl_DZ
-print('Курс:',Course_name,', всего задач: ',Num_of_compl_DZ,', затрачено часов: ',Num_of_hours,'среднее время выполнения ',Time_for_one_task)
+print('Курс:',Course_name,', всего задач: ',Num_of_compl_DZ,', затрачено часов: ',Num_of_hours,', среднее время выполнения ',Time_for_one_task,'часа')
 
