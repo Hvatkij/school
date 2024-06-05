a281ec95b8e83c7ce520903214276de4023adcab
Хваткий <ivan-zapara@ya.ru>
2024-06-06 02:44:31 +0300

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,19 @@
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+num = 0
+while(num < len(my_list)):
+    if my_list[num] > 0:    # selection positive num
+        print(my_list[num], end = " ")
+    elif my_list[num] < 0:  # filtr
+        break
+    num += 1                 # increment
+
+
+
+
+
+
+
+
+
+# while
